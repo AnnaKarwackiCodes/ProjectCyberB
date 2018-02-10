123cfacb27baed008df41bbcfb5d5388a0c439f5
avk9238 <avk9238@rit.edu>
2018-02-10 13:34:18 -0500

    Setting up base project
    The project is now set up to make a VR game. Yay

---
+++ ProjectCyberBall/Assets/scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+	public float TurnRate = 90f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(
+			Vector3.up *
+			Time.deltaTime *
+			TurnRate);
+	}
+}
