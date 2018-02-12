1270ba5a3d6cc09eba0d573c6b805ad7d96b63c6
avk9238 <avk9238@rit.edu>
2018-02-11 21:19:05 -0500

    oh boy we got controller tracking
    controller tracking is now in the project. to actually get it to work is
    weird and apparently a unity/hardware problem. so when you have the
    whole VR set you need to have the headset semi on and mouse clicking and
    holding on the play button, release and put on headset fully at same
    time. its really freaking dumb

---
+++ ProjectCyberBall/Assets/scripts/MotionControllers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.UI;
+
+public class MotionControllers : MonoBehaviour {
+
+	public GameObject lefty;
+	public GameObject righty;
+	public GameObject textBox;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//how to get the left controller's position
+		Vector3 leftPosition = InputTracking.GetLocalPosition (XRNode.LeftHand);
+		Quaternion leftRotation = InputTracking.GetLocalRotation (XRNode.LeftHand);
+
+		//how to get the right controller's position
+		Vector3 rightPosition = InputTracking.GetLocalPosition (XRNode.RightHand);
+		Quaternion rightRotation = InputTracking.GetLocalRotation (XRNode.RightHand);
+
+		//Debug.Log (leftPosition);
+		//Debug.Log (leftRotation);
+
+		textBox.GetComponent<Text>().text = leftPosition + " " + leftRotation;
+
+		//moving the left "hand"
+		lefty.transform.localPosition = leftPosition;
+		lefty.transform.localRotation = leftRotation;
+
+		//moving the right "hand"
+		righty.transform.localPosition = rightPosition;
+		righty.transform.localRotation = rightRotation;
+	}
+}
