2958baf900a72aba833e69aab4b0c6f3132271f2
avk9238 <avk9238@rit.edu>
2018-02-21 18:30:25 -0500

    ray casting is in

--- ProjectCyberBall/Assets/scripts/RayCasting.cs
+++ ProjectCyberBall/Assets/scripts/RayCasting.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RayCasting : MonoBehaviour {
 
 	LineRenderer line;
+	RaycastHit hit;
+
+	public Text txt;
 	// Use this for initialization
 	void Start () {
 		line = gameObject.GetComponent<LineRenderer> ();
@@ -14,11 +18,19 @@ public class RayCasting : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetAxis ("Right_Trigger") == 1.0f) {
+			//drawing the line to show what its interacting with
 			line.enabled = true;
 			Ray ray = new Ray (transform.position, transform.forward);
 
 			line.SetPosition (0, ray.origin);
 			line.SetPosition (1, ray.GetPoint (10));
+
+			//now we are going to see if we are colliding with something
+			if (Physics.Raycast (ray, out hit, 10)) {
+				if (hit.collider.gameObject.tag == "Hex") {
+					txt.text = "hex";
+				}
+			} 
 		}
 		else {
 			line.enabled = false;
