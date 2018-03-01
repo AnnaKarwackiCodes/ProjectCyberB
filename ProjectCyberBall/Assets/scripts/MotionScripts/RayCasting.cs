ae1e47547023f702a20bb1072ebec3f70362edf4
avk9238 <avk9238@rit.edu>
2018-03-01 04:46:04 -0500

    Player Class features
    Player is able to teleport to a Hex that they select.
    Player is able to summon both a Big boi and a smol Boi.
    these additions have some temp code within it for testing purposes, but
    it still includes the proper code for the actual game.
    all that remains really for the player class is attacks(fireball and
    punch), pick up ball and pass(that will take no time at all), and
    interacting with the minions on the field to do things (move, attack).
    But with having move and summoning working the others will closely
    follow.
    
    ALSO IF YOU HAVE TO CHANGE THE BUILD SETTING PLEASE PUT SOME SORT OF
    MESSAGE IN YOU COMMIT PLEASE AND THANKS

--- ProjectCyberBall/Assets/scripts/MotionScripts/RayCasting.cs
+++ ProjectCyberBall/Assets/scripts/MotionScripts/RayCasting.cs
@@ -8,7 +8,7 @@ public class RayCasting : MonoBehaviour {
 	LineRenderer line;
 	RaycastHit hit;
 
-	public Text txt;
+	//public Text txt;
 	// Use this for initialization
 	void Start () {
 		line = gameObject.GetComponent<LineRenderer> ();
@@ -17,6 +17,7 @@ public class RayCasting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        /*
 		if (Input.GetAxis ("Right_Trigger") == 1.0f) {
 			//drawing the line to show what its interacting with
 			line.enabled = true;
@@ -28,12 +29,46 @@ public class RayCasting : MonoBehaviour {
 			//now we are going to see if we are colliding with something
 			if (Physics.Raycast (ray, out hit, 10)) {
 				if (hit.collider.gameObject.tag == "Hex") {
-					txt.text = "hex";
+                    // if the player has selected the move function then move to that object
 				}
+                if(hit.collider.gameObject.tag == "Ally")
+                {
+                    // asked the player what they want the ally to do:
+                    //move, attack, pass ball
+                }
 			} 
 		}
 		else {
 			line.enabled = false;
 		}
+        */
 	}
+
+    //should have a function that changes the range that the raycast is checking
+    public void SelectingObj(float distance, string wantedTag)
+    {
+        Debug.Log("selecting obj");
+        //drawing the line to show what it's interacting with
+        line.enabled = true;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        line.SetPosition(0, ray.origin);
+        line.SetPosition(1, ray.GetPoint(distance));
+
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            if (hit.collider.gameObject.tag == wantedTag && Input.GetAxis("Right_Trigger") == 1.0f)
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<playerScript>().SelectedObj = hit.collider.gameObject;
+                line.enabled = false;
+                
+            }
+        }
+        
+    }
+
+    public bool Line
+    {
+        set { line.enabled = value; }
+    }
 }
